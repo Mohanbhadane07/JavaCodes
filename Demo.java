442a1f19a940c77f95ecb6c286c04ae2da1512f9
Mohanbhadane07 <mohanbhadane0@gmail.com>
2025-08-25 23:33:21 +0530

    add file

--- Demo.java
+++ Demo.java
@@ -9,7 +9,7 @@ public class Demo {
   public static void main(String[] args) {
     student s1 = new student();
     s1.rollno = 1;
-    s1.name = "mohan";
+    s1.name = "mohan"; 
     s1.marks = 90;
 
     student s2 = new student();
@@ -27,8 +27,8 @@ public class Demo {
     students[1] = s2;
     students[2] = s3;
 
-    for(int i = 0; i < students.length; i++) {
-      System.out.println(students[i].rollno + " " + students[i].name + " " + students[i].marks);
+    for ( student n : students) {
+      System.out.println(n.marks + " " + n.name + " " + n.rollno);
     }
   }
 }
